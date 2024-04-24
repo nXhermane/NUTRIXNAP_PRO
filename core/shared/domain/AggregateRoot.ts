db874b3589748b51cb32b22dd971c80a335e01a7
nXhermane <142807581+nXhermane@users.noreply.github.com>
2024-04-24 11:53:44 +0100

    improve

--- core/shared/domain/AggregateRoot.ts
+++ core/shared/domain/AggregateRoot.ts
@@ -1,9 +1,21 @@
 import { Entity, CreateEntityProps } from "./Entity";
 
+export abstract class AggregateRoot<EntityProps extends object> extends Entity<EntityProps> {
+  private _eventHandlers: Array<() => void> = [];
 
-
-export abstract class AggregateRoot<EntityProps> extends Entity<EntityProps> {
-    public publishEvents() {
-        console.log("Event publier par: ", this.constructor.name);
+  constructor(props: CreateEntityProps<EntityProps>, eventHandlers?: Array<() => void>) {
+    super(props);
+    if (eventHandlers) {
+      this._eventHandlers.push(...eventHandlers);
     }
+  }
+
+  public publishEvents() {
+    console.log(`Events published by: ${this.constructor.name}`);
+    this._eventHandlers.forEach((handler) => handler());
+  }
+
+  public subscribe(handler: () => void) {
+    this._eventHandlers.push(handler);
+  }
 }
