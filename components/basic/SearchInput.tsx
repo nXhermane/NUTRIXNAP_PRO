8046add739f9f70cfc20f7a264db29f6d6ce7c11
nXhermane <142807581+nXhermane@users.noreply.github.com>
2024-04-24 11:53:50 +0100

    improve

--- components/basic/SearchInput.tsx
+++ components/basic/SearchInput.tsx
@@ -1,95 +1,95 @@
-import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
-import {ThemeInterface,useTheme,useThemeStyles} from "@/theme"
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  useWindowDimensions
+} from "react-native";
+import {
+  ThemeInterface,
+  useTheme,
+  useThemeStyles
+} from "@/theme";
 import { MaterialIcons } from "@expo/vector-icons";
+import { PressableProps } from "react-native";
+import { TextInputProps } from "react-native";
+import { ViewProps } from "react-native";
+import { TextProps } from "react-native";
+import { ThemeConsumerProps } from "@/theme";
+
 interface Props {
-    // Define your props here
+  // Define your props here
+  onBlur?: () => void;
+  onFocus?: () => void;
+  isFocus?: boolean;
+  disableClearIcon?: boolean;
+  h?: number;
+  w?: number;
+  r?: number;
+  bw?: number;
+  st?: any;
+  v?: string;
+  onChange?: (text: string) => void;
+  p?: string;
+  pC?: string;
+  iM?: "text" | "decimal" | "numeric" | "email" | "phone" | undefined;
+  ref?: any;
+  onClear?: () => void;
+  autoCorrect?: boolean;
 }
 
 const SearchInput = (props: Props) => {
-    const {
-        onBlur = () => {},
-        onFocus = () => {},
-        isFocus = false,
-        disableClearIcon = true,
-        h,
-        w,
-        r,bw,
-        st
-    } = props;
-    const { colors, size } = useTheme();
-    const style = useThemeStyles(styles);
-    return (
-        <View style={style.search({h,w,r,bw,st})}>
-            <View style={style.iconContainer}>
-                <MaterialIcons name={"search"} size={24} color={colors.gray} />
-            </View>
-            <View style={style.textInputContainer}>
-                <TextInput
-                    style={style.textInput}
-                    value={props.v}
-                    onChangeText={props.onChange}
-                    placeholder={props.p || "Search..."}
-                    placeholderTextColor={props.pC}
-                    autoCorrect={true}
-                    // onSubmitEditing={({nativeEvent: {text, eventCount, target}})=>buttonPress()}
-                    inputMode={props.iM || "text"}
-                    ref={props.ref}
-                    onBlur={() => onBlur()}
-                    onFocus={() => {
-                        onFocus();
-                    }}
-                />
-            </View>
-            {disableClearIcon && (
-                <Pressable
-                    style={style.iconContainer}
-                    onPress={() => props.onClear()}
-                >
-                    <MaterialIcons
-                        name={"clear"}
-                        size={15}
-                        color={colors.gray}
-                    />
-                </Pressable>
-            )}
-        </View>
-    );
-};
-
-export default SearchInput;
-
-const styles = ({ size, colors }:ThemeInterface) =>
-    StyleSheet.create({
-        search: ({ h, w, r, bw,st }) => ({
-            width: w || size.width * 0.8,
-            height: h || size.s50 * 0.9,
-            borderRadius: r || size.s100 * 2,
-            backgroundColor: colors.bg.bg2,
-            //  borderWidth:bw|| size.s1/6,
-            borderColor: colors.gray,
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-            overflow: "hidden",
-            paddingHorizontal: size.s2,
-            elevation:1,
-            ...st
-        }),
-        textInputContainer: {
-            flex: 8,
-            height: "100%"
-        },
-        textInput: {
-            height: "100%",
-            fontFamily: "inter",
-            fontSize: size.s3 * 1.1,
-            fontWeight: "600",
-            color: colors.text.primary+'95'
-        },
-        iconContainer: {
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%"
-        }
-    });
+  const {
+    onBlur = () => {},
+    onFocus = () => {},
+    isFocus = false,
+    disableClearIcon = true,
+    h,
+    w,
+    r,
+    bw,
+    st,
+    v,
+    onChange,
+    p = "Search...",
+    pC = "gray",
+    iM = "text",
+    ref,
+    onClear,
+    autoCorrect = true
+  } = props;
+  const { colors, size } = useTheme();
+  const style = useThemeStyles(styles);
+  const { width } = useWindowDimensions();
+  return (
+    <View
+      style={style.search({
+        h: h || width * 0.1,
+        w: w || width * 0.8,
+        r: r || size.s100 * 2,
+        bw: bw || size.s1 / 6,
+        st: st || {}
+      })}
+    >
+      <View style={style.iconContainer}>
+        <MaterialIcons name={"search"} size={24} color={colors.gray} />
+      </View>
+      <View style={style.textInputContainer}>
+        <TextInput
+          style={style.textInput}
+          value={v}
+          onChangeText={onChange}
+          placeholder={p}
+          placeholderTextColor={pC}
+          autoCorrect={autoCorrect}
+          inputMode={iM}
+          ref={ref}
+          onBlur={() => onBlur()}
+          onFocus={() => {
+            onFocus();
+          }}
+        />
+      </View>
+      {disableClearIcon && (
+        <Pressable
